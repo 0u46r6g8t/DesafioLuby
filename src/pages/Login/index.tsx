bce5d8dfd0621b6a8ee1f806223efdc4d830490c
Gustavo Silva Quieregato <quieregatog@alunos.utfpr.edu.br>
2021-05-03 09:06:40 -0300

    Atualizando

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable react/void-dom-elements-no-children */
 /* eslint-disable consistent-return */
 /* eslint-disable @typescript-eslint/no-empty-function */
